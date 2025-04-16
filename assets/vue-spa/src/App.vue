136a293e7cb5448cd7ce1e8d9b9f71251198df5a
David Schönhofen <d.schoenhofen.dev@gmail.com>
2025-04-16 23:24:43 +0200

    Setup Vue SPA

---
+++ assets/vue-spa/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import TheHeader from "@/components/TheHeader.vue";
+</script>
+
+<template>
+  <the-header />
+  <router-view />
+</template>
---
+++ assets/vue-spa/src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, useRouter } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li v-for="route in useRouter().getRoutes()" :key="route.path">
+          <router-link :to="route.path">{{ route.name }}</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
